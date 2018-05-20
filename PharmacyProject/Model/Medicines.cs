f25ef16a72e83e8df7be79241450e623385c6264
Magda Pelikant <magda.pelikant@gmail.com>
2018-05-20 16:22:28 +0200

    Update procedures and create project

---
+++ PharmacyProject/Model/Medicines.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Medicines
+    {
+        public int medicineid { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal unit_price { get; set; }
+        public DateTime unit_price_change_date { get; set; }
+    }
+}
